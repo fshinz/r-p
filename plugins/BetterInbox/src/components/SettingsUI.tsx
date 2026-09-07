9ff65ffedfe9050c434e8d7bfab9313045eb53a1
shin <mshin3400@gmail.com>
2026-09-07 21:52:52 +0300

    Update SettingsUI.tsx

--- plugins/BetterInbox/src/components/SettingsUI.tsx
+++ plugins/BetterInbox/src/components/SettingsUI.tsx
@@ -2,6 +2,7 @@ import { React, ReactNative as RN } from "@vendetta/metro/common";
 import { findByProps } from "@vendetta/metro";
 import { storage } from "@vendetta/plugin";
 import { useProxy } from "@vendetta/storage";
+import { refreshYouBarUI } from "../index";
 
 const { ScrollView } = RN;
 
@@ -17,6 +18,11 @@ export default function SettingsUI(): JSX.Element {
     const showSettings = storage.showSettingsButton ?? true;
     const showInbox = storage.showInboxButton ?? true;
 
+    const handleToggle = (key: string, val: boolean) => {
+        storage[key] = val;
+        refreshYouBarUI();
+    };
+
     return (
         <ScrollView style={{ flex: 1 }} contentContainerStyle={{ padding: 12 }}>
             <Stack spacing={16}>
@@ -26,25 +32,19 @@ export default function SettingsUI(): JSX.Element {
                             label="Direct Messages Button"
                             subLabel="Show quick jump button to DM channel list"
                             value={showDM}
-                            onValueChange={(val: boolean) => {
-                                storage.showDMButton = val;
-                            }}
+                            onValueChange={(val: boolean) => handleToggle("showDMButton", val)}
                         />
                         <TableSwitchRow
                             label="App Settings Button"
                             subLabel="Show quick button to open Discord settings"
                             value={showSettings}
-                            onValueChange={(val: boolean) => {
-                                storage.showSettingsButton = val;
-                            }}
+                            onValueChange={(val: boolean) => handleToggle("showSettingsButton", val)}
                         />
                         <TableSwitchRow
                             label="BetterInbox Button"
                             subLabel="Show notification center icon in YouBar"
                             value={showInbox}
-                            onValueChange={(val: boolean) => {
-                                storage.showInboxButton = val;
-                            }}
+                            onValueChange={(val: boolean) => handleToggle("showInboxButton", val)}
                         />
                     </TableRowGroup>
                 ) : null}
