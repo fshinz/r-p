2916e41e7609624425f1fd983bed161377f31375
shin <mshin3400@gmail.com>
2026-09-07 23:18:04 +0300

    Update youbar.tsx

--- plugins/BetterInbox/src/youbar.tsx
+++ plugins/BetterInbox/src/youbar.tsx
@@ -1,7 +1,7 @@
 import { logger } from "@vendetta";
 import { findByProps, findByName, findByTypeName } from "@vendetta/metro";
 import { React } from "@vendetta/metro/common";
-import { instead } from "@vendetta/patcher";
+import { after } from "@vendetta/patcher";
 import { storage } from "@vendetta/plugin";
 import { getAssetIDByName } from "@vendetta/ui/assets";
 import NotificationCenterUI from "./components/NotificationCenterUI";
@@ -43,10 +43,10 @@ export function patchYouBar(): () => void {
         ));
     };
 
-    logger.log("[BetterInbox] Patching YouBarNotificationsButton.type...");
+    logger.log("[BetterInbox] Patching YouBarNotificationsButton.type using after...");
 
-    return instead("type", YouBarNotificationsButton, (args, OriginalRender) => {
-        const res = OriginalRender(...args);
+    // `after` receives (args, res) where `res` is the React Element returned by Original render
+    return after("type", YouBarNotificationsButton, (args, res) => {
         if (!res?.props?.children) return res;
 
         const IconButton = res.props.children.type;
