5099c1244565eb90c40df4272ec68589ce10390a
shin <mshin3400@gmail.com>
2026-09-09 16:42:32 +0300

    Update index.tsx

--- plugins/BetterInbox/src/index.tsx
+++ plugins/BetterInbox/src/index.tsx
@@ -1,15 +1,10 @@
 import { logger } from "@vendetta";
-import { findByProps } from "@vendetta/metro";
 import { storage } from "@vendetta/plugin";
-import { rescanAndPatchYouBar, resetYouBarPatchState, forceNavigationRerender } from "./youbar";
+import { linkYouBarAnimation, resetYouBarPatchState } from "./youbar";
 import { initNotificationEngine, stopNotificationEngine } from "./notifications";
 import SettingsUI from "./components/SettingsUI";
 
 const cleanups: (() => void)[] = [];
-
-export function refreshYouBarUI() {
-    forceNavigationRerender();
-}
 
 export default {
     onLoad: () => {
@@ -21,41 +16,18 @@ export default {
 
         initNotificationEngine();
 
-        // 1. Initial scanning loop
+        // High-frequency check to bind the YouBar animation module as early as possible
         let attempts = 0;
         const scanInterval = setInterval(() => {
-            const patched = rescanAndPatchYouBar(cleanups);
+            const success = linkYouBarAnimation(cleanups);
             attempts++;
-            if (patched || attempts > 60) {
+
+            if (success || attempts > 100) {
                 clearInterval(scanInterval);
             }
-        }, 100);
+        }, 50);
+
         cleanups.push(() => clearInterval(scanInterval));
-
-        // 2. Link directly to React Navigation State changes
-        const NavigationContainer = findByProps("useNavigationContainerRef", "NavigationContainer") || findByProps("getRootRef");
-        const FluxDispatcher = findByProps("dispatch", "subscribe");
-
-        const handleNavigationChange = () => {
-            // Run hook scanner and issue the re-render dispatch
-            rescanAndPatchYouBar(cleanups);
-            forceNavigationRerender();
-        };
-
-        if (FluxDispatcher?.subscribe) {
-            // Triggers every time you switch tabs, open channels, or change screens
-            FluxDispatcher.subscribe("NAVIGATION_SWITCH", handleNavigationChange);
-            FluxDispatcher.subscribe("CHANNEL_SELECT", handleNavigationChange);
-            FluxDispatcher.subscribe("TRACK", handleNavigationChange);
-
-            cleanups.push(() => {
-                FluxDispatcher.unsubscribe("NAVIGATION_SWITCH", handleNavigationChange);
-                FluxDispatcher.unsubscribe("CHANNEL_SELECT", handleNavigationChange);
-                FluxDispatcher.unsubscribe("TRACK", handleNavigationChange);
-            });
-        }
-
-        forceNavigationRerender();
     },
 
     onUnload: () => {
